2c4dcc0409fb132205b5e4f4bb58c7add1e16b1b
James Basile <jbasile14@gmail.com>
2019-02-27 19:19:46 -0500

    passed in data to render components in componentDidMount function, step 6 complete

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class Friends extends React.Component {
     constructor() {
@@ -8,10 +9,23 @@ class Friends extends React.Component {
         }
     }
 
+    componentDidMount() {
+        axios
+            .get('http://localhost:5000/friends')
+            .then( res => this.setState({ friends: res.data }))
+            .catch( err => this.setState({ error: err}));
+    }
+
     render() {
         return (
             <div className='friends-container'>
-            <p>FRIENDSFRIENDSFRIENDS</p>
+            {this.state.friends.map(friend => (
+                <div className='eachFriend'>
+                    <h2> {friend.name} </h2>
+                    <p><strong>Age:</strong> {friend.age} </p>
+                    <p><strong>Email:</strong> {friend.email} </p>
+                </div>
+            ))}
             </div>
         )
     }
